f49a1dfdcddff34dea2ed5341d3a9905ed0d0dc8
MichelleBKlynn <michellestaton@gmail.com>
2016-09-07 09:31:16 -0400

    Complete answers

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -6,22 +6,36 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
 
-// Our blank piece of paper
-var shoppingList: [String] = []
+    // Our blank piece of paper
+    var shoppingList: [String] = []
 
-// Quantity of items
-var quantityOfItems: [Int] = [6, 4, 12, 4]
+    // Quantity of items
+    var quantityOfItems: [Int] = [6, 4, 12, 4]
 
-// Name of items
-var nameOfItems: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
-
-
+    // Name of items
+    var nameOfItems: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
 
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+ makeShoppingListWithItems(nameOfItems, quantityOfItems: quantityOfItems)
+//
+    }
+//
+//    // This is just an example - you can remove this.
+//    func thisIsAnExample() {
+//        print("This ist just an example, you can remvove this.")
+//    }
+//
+//
 func makeShoppingListWithItems(_:[String], quantityOfItems: [Int]) -> [String] {
     for (index, value) in quantityOfItems.enumerate() {
         shoppingList.append("\(index + 1). \(value) \(nameOfItems[index])")
@@ -31,16 +45,5 @@ func makeShoppingListWithItems(_:[String], quantityOfItems: [Int]) -> [String] {
     
 }
 
-    
 
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-makeShoppingListWithItems(nameOfItems, quantityOfItems: quantityOfItems)
-        
-    }
-    
-
-}
+ }
